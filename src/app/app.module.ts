1ca7b72b6a38ee8f850912c4cc2e482350c7a33f
josesalass <jose.salas1801@alumnos.ubiobio.cl>
2022-05-08 16:32:21 -0400

    actualización a componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
